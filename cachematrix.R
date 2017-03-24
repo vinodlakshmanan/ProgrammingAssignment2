8913da51cda7867b18dd15c9d2cb14aa23feb2e5
Vinod Lakshmanan <vinodlakshmanan@gmail.com>
2017-03-24 13:56:34 +0530

    Updated R script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This script provides an implementation of a cache
+# which holds the inverse of a given matrix
+# makeCacheMatrix <- Object which describes a "cache record"
+# cacheSolve <- Functions that builds / queries the cache
+# Sample Test case:
+# a <- makeCacheMatrix(matrix(1:4 ,2 ,2))
+# a$getinv() ... should return NULL
+# cacheSolve(a) ... should calculate and return inverse
+# cacheSolve(a) ... shoule return cached value
+# a$getinv() ... should return the cached inverse
 
-## Write a short comment describing this function
+###############################################################
+# makeCacheMatrix is the implementation of a "Cache Record"
+# It is a self-contained object, which holds both the matrix
+# and its inverse. The function returns a list
+# containing mutator and accessor (getters and setters) for the
+# main matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get
+       , setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve is the entry point function for the cache
+# It takes an object of type "makeCacheMatrix" and
+# 1. CHecks if the matrix is square
+# 2. If square, Returns the inverse if it exists; if not square, throws up an error
+# 3. Calculates, sets and returns the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("Getting cached data")
+    return(inv)
+  }
+  input <- x$get()
+  inv <- solve(input,...)
+  x$setinv(inv)
+  inv
+
 }
